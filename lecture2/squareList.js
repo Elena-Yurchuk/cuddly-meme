c654f92e9f010632a0b501f3a33e02d76a4ea9ee
Yurchuk Elena <elena.moonshine.xxx@gmail.com>
2022-02-06 21:11:45 +0200

    solved task 15 with the help of reduce() method

--- lecture2/squareList.js
+++ lecture2/squareList.js
@@ -2,8 +2,13 @@ export const squareList = (numbers) => {
   if (!Array.isArray(numbers)) {
     return 'Error: wrong type of argument';
   }
+  
+  return numbers.reduce((total, number) => {
+    if (Number.isInteger(number) && Math.sign(number) > 0) {
+      total.push(Math.pow(number, 2));
+    }
 
-  return numbers
-    .filter((number) => Number.isInteger(number) && Math.sign(number) > 0)
-    .map(number => Math.pow(number, 2));
+    return total;
+    
+  }, []);
 };
