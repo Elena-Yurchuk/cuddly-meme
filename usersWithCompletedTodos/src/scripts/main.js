46c6319b1fc04189250ddad5f186cfc743a2d215
Yurchuk Elena <elena.moonshine.xxx@gmail.com>
2022-02-14 22:04:24 +0200

    created function with usersWithCompletedTodos

--- usersWithCompletedTodos/src/scripts/main.js
+++ usersWithCompletedTodos/src/scripts/main.js
@@ -4,22 +4,27 @@ const usersEndpoint = 'https://jsonplaceholder.typicode.com/users';
 const todosEndpoint = 'https://jsonplaceholder.typicode.com/todos';
 
 async function getUsers() {
-  const usersRes = await fetch(usersEndpoint);
-  const todosRes = await fetch(todosEndpoint);
-
-  const todos = await todosRes.json();
-  const users = await usersRes.json();
-
-  const allUsers = users.map((user) => {
-    return {
-      ...user,
-      todos: todos
-        .filter((todo) => user.id === todo.userId)
-        .filter((todo) => todo.completed === true),
-    };
-  });
-
-  getUsersWithCompletedTodos(allUsers);
+  try {
+    const usersRes = await fetch(usersEndpoint);
+    const todosRes = await fetch(todosEndpoint);
+  
+    const todos = await todosRes.json();
+    const users = await usersRes.json();
+  
+    const allUsers = users.map((user) => {
+      return {
+        ...user,
+        todos: todos
+          .filter((todo) => user.id === todo.userId)
+          .filter((todo) => todo.completed === true),
+      };
+    });
+  
+    getUsersWithCompletedTodos(allUsers);
+  } catch (error) {
+    throw new Error ('Error');
+  }
+  
 
 }
 
