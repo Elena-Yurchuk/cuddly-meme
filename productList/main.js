e1bb40a017a6e2a01b66c0f1672a963592395839
Elena <olena.yurchuk@niko-tech.eu>
2022-02-15 15:52:49 +0200

    deleted console.log

--- productList/main.js
+++ productList/main.js
@@ -1,14 +1,15 @@
 const productsEndpoint = 'https://fakestoreapi.com/products';
 
-const sortedList = async () => {
+const sortedListOfProducts = async () => {
   try {
     const listResponse = await fetch(productsEndpoint);
+
     const list = await listResponse.json();
+    
     list.sort((a, b) => (a.category > b.category) ? 1 : (a.category === b.category) ? ((a.price < b.price) ? 1 : -1) : -1);
-    console.log(list)
   } catch (err) {
     throw new Error('Error: ', err);
   }
 };
 
-sortedList();
+sortedListOfProducts();
