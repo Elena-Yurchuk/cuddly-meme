91cd39b23c5c32a24494f05584803f447d94bc70
Elena <olena.yurchuk@niko-tech.eu>
2022-02-15 16:06:14 +0200

    added return to function

--- usersWithCompletedTodos/src/scripts/main.js
+++ usersWithCompletedTodos/src/scripts/main.js
@@ -2,7 +2,7 @@ const usersEndpoint = 'https://jsonplaceholder.typicode.com/users';
 
 const todosEndpoint = 'https://jsonplaceholder.typicode.com/todos';
 
-const getUsersWithCompletedTodos = async () => {
+const getUsersWithCompletedTodos = async() => {
 
   const usersRes = await fetch(usersEndpoint);
 
@@ -20,6 +20,8 @@ const getUsersWithCompletedTodos = async () => {
         .filter((todo) => todo.completed === true),
     };
   });
-}
+
+  return allUsers;
+};
 
 getUsersWithCompletedTodos(); 
