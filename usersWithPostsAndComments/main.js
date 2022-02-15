3a8fd4490cb013abc0afd002e1254e7aad065e37
Elena <olena.yurchuk@niko-tech.eu>
2022-02-15 16:19:00 +0200

    created function getUsersWithPostsAndComments

---
+++ usersWithPostsAndComments/main.js
@@ -0,0 +1,40 @@
+const usersEndpoint = 'https://jsonplaceholder.typicode.com/users';
+
+const postsEndpoint = 'https://jsonplaceholder.typicode.com/posts';
+
+const commentsEndpoint = 'https://jsonplaceholder.typicode.com/comments';
+
+const getUsersWithPostsAndComments = async() => {
+  try {
+    const usersRes = await axios.get(usersEndpoint);
+
+    const postsRes = await axios.get(postsEndpoint);
+
+    const commentsRes = await axios.get(commentsEndpoint);
+
+    const users = usersRes.data;
+
+    const posts = postsRes.data;
+
+    const comments = commentsRes.data;
+
+    const allUsers = users.map((user) => {
+      return {
+        ...user,
+        posts: posts
+          .filter((post) => user.id === post.userId)
+          .map((post) => ({
+            ...post,
+            comments: comments
+              .filter((comment) => post.id === comment.postId),
+          })),
+      };
+    });
+
+    return allUsers;
+  } catch (error) {
+    throw new Error ('Error');
+  }
+};
+
+getUsersWithPostsAndComments();
