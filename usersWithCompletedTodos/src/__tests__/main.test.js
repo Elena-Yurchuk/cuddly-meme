a0cc1156dc31809c9306ad0e2a471f0968190473
Elena <olena.yurchuk@niko-tech.eu>
2022-02-21 17:38:07 +0200

    added try...catch statement

--- usersWithCompletedTodos/src/__tests__/main.test.js
+++ usersWithCompletedTodos/src/__tests__/main.test.js
@@ -11,61 +11,63 @@ beforeEach(() => {
 });
 
 describe('getUsersWithCompletedTodos', () => {
-  const users = [
-    {
-      id: 7, name: 'Elena',
-    },
-  ];
-
-  const todos = [
-    {
-      userId: 7,
-      title: 'delectus aut autem',
-      id: 1,
-      completed: true,
-    },
-  ];
-
-  beforeEach(() => {
-    fetch.mockImplementationOnce(() => Promise.resolve({
-      json: () => Promise.resolve(users),
-    }));
-    
-
-    fetch.mockImplementationOnce(() => Promise.resolve({
-      json: () => Promise.resolve(todos),
-    }));
-
-  });
-
-  test('getUsersWithCompletedTodos', async() => {
-    const result = await getUsersWithCompletedTodos();
-
-    expect(result).toEqual([{
-      id: 7, name: 'Elena', todos,
-    }]);
-  });
-
-  test('fetch should be called twice', async() => {
-    fetch.mockImplementationOnce(() => Promise.resolve({
-      json: () => Promise.resolve(users),
-    }));
-    
-
-    fetch.mockImplementationOnce(() => Promise.resolve({
-      json: () => Promise.resolve(todos),
-    }));
-    
-    await getUsersWithCompletedTodos();
-
-    expect(fetch).toHaveBeenCalledTimes(2);
-
-    expect(fetch).toHaveBeenCalledWith(
-      'https://jsonplaceholder.typicode.com/users',
-    );
-
-    expect(fetch).toHaveBeenCalledWith(
-      'https://jsonplaceholder.typicode.com/todos',
-    );
+  describe('when API is successful', () => {
+    const users = [
+      {
+        id: 7, name: 'Elena',
+      },
+    ];
+  
+    const todos = [
+      {
+        userId: 7,
+        title: 'delectus aut autem',
+        id: 1,
+        completed: true,
+      },
+    ];
+  
+    beforeEach(() => {
+      fetch.mockImplementationOnce(() => Promise.resolve({
+        json: () => Promise.resolve(users),
+      }));
+      
+  
+      fetch.mockImplementationOnce(() => Promise.resolve({
+        json: () => Promise.resolve(todos),
+      }));
+  
+    });
+  
+    test('getUsersWithCompletedTodos', async() => {
+      const result = await getUsersWithCompletedTodos();
+  
+      expect(result).toEqual([{
+        id: 7, name: 'Elena', todos,
+      }]);
+    });
+  
+    test('fetch should be called twice', async() => {
+      fetch.mockImplementationOnce(() => Promise.resolve({
+        json: () => Promise.resolve(users),
+      }));
+      
+  
+      fetch.mockImplementationOnce(() => Promise.resolve({
+        json: () => Promise.resolve(todos),
+      }));
+      
+      await getUsersWithCompletedTodos();
+  
+      expect(fetch).toHaveBeenCalledTimes(2);
+  
+      expect(fetch).toHaveBeenCalledWith(
+        'https://jsonplaceholder.typicode.com/users',
+      );
+  
+      expect(fetch).toHaveBeenCalledWith(
+        'https://jsonplaceholder.typicode.com/todos',
+      );
+    });
   });
 });
--- usersWithCompletedTodos/src/scripts/main.js
+++ usersWithCompletedTodos/src/scripts/main.js
@@ -7,24 +7,28 @@ const USERS_ENDPOINT = 'https://jsonplaceholder.typicode.com/users';
 const TODOS_ENDPOINT = 'https://jsonplaceholder.typicode.com/todos';
 
 export const getUsersWithCompletedTodos = async() => {
-  const [usersRes, todosRes] = await Promise.all([
-    fetch(USERS_ENDPOINT),
-    fetch(TODOS_ENDPOINT),
-  ]);
-
-  const todos = await todosRes.json();
-
-  const users = await usersRes.json();
-
-  const allUsers = users.map((user) => {
-    return {
-      ...user,
-      todos: todos
-        .filter((todo) => user.id === todo.userId && todo.completed),   
-    };
-  });
-
-  return allUsers;
+  try {
+    const [usersRes, todosRes] = await Promise.all([
+      fetch(USERS_ENDPOINT),
+      fetch(TODOS_ENDPOINT),
+    ]);
+  
+    const todos = await todosRes.json();
+  
+    const users = await usersRes.json();
+  
+    const allUsers = users.map((user) => {
+      return {
+        ...user,
+        todos: todos
+          .filter((todo) => user.id === todo.userId && todo.completed),   
+      };
+    });
+  
+    return allUsers;
+  } catch (error) {
+    throw new Error ('Network Error');
+  }
 };
  
 (async() => {
